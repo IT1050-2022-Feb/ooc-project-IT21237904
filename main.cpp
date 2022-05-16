54add59e746a46079cedb7fc3825dda5dcd70bc0
IT21237904 <99158714+IT21237904@users.noreply.github.com>
2022-05-16 04:53:00 +0000

    modification

--- main.cpp
+++ main.cpp
@@ -13,13 +13,10 @@ int main() {
 
 //create objects here--------------------------------------------
 
-    Individual i1("p001","goldIndividual","best plan for individual",35000);  
-    Individual i2("p002","silverIndividual","good plan for individual",25000);  
-    Individual i3("p003","bronzeIndividual","basic plan for individual",15000);  
-    
-    Group g1("p004","goldGroup","best plan for individual",500000); 
-    Family f1("p005","goldFamily","best plan for individual",100000);  
-    SeniorCitizen s1("p006","goldSeniorCitizen","best plan for individual",25000);  
+    Individual i1("p001","goldIndividual","best plan for individual",35000);   
+    Group g1("p002","goldGroup","best plan for individual",500000); 
+    Family f1("p003","goldFamily","best plan for individual",100000);  
+    SeniorCitizen s1("p004","goldSeniorCitizen","best plan for individual",25000);  
 
 
 
@@ -27,9 +24,7 @@ int main() {
 
 
 //call functions------------------------------------------------ 
-    i1.	displayDetails(); 
-    i2.	displayDetails(); 
-    i3.	displayDetails(); 
+    i1.	displayDetails();  
     g1.	displayDetails(); 
     f1.	displayDetails(); 
     s1.	displayDetails(); 
